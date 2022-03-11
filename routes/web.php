40cce2db80cdb28705d7064dbdb2d8bb69fbb318
FcoGzlz <franciscogzlz533@gmail.com>
2022-03-11 12:21:26 -0300

    agregada la ruta al contralador para realizar pruebas

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ use Spatie\Permission\Contracts\Role;
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
+
+Route::get('/', [App\Http\Controllers\TestCntroller::class, 'index']);
 
 Auth::routes();
 
